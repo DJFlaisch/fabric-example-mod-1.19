2a5830ccfd3bdb0a999c28b140b9a7fafc3b2250
Djflaisch <djflaisch1@gmail.com>
2023-03-26 17:39:03 -0400

    a

---
+++ src/main/java/net/djflaisch/testingmod/TestingMod.java
@@ -0,0 +1,15 @@
+package net.djflaisch.testingmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestingMod implements ModInitializer {
+	public static final String MOD_ID = "testingmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
